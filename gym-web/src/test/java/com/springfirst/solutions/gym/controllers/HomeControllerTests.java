2dc35e61017bd17bf91db992ae8d07dfb59b7a1d
peter greaves <peter.greaves64@gmail.com>
2021-08-11 10:06:34 +0100

    security test support added closes #28

--- gym-web/src/test/java/com/springfirst/solutions/gym/controllers/HomeControllerTests.java
+++ gym-web/src/test/java/com/springfirst/solutions/gym/controllers/HomeControllerTests.java
@@ -1,24 +1,80 @@
 package com.springfirst.solutions.gym.controllers;
 
+import com.springfirst.solutions.gym.services.ImageService;
+import com.springfirst.solutions.gym.services.TrainerService;
+import com.springfirst.solutions.gym.services.TrainerSpecialityService;
+import org.h2.server.web.WebApp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest
 public class HomeControllerTests {
 
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @MockBean
+    ImageService imageService;
+
+    @MockBean
+    TrainerService trainerService;
+
+    @MockBean
+    TrainerSpecialityService trainerSpecialityService;
+
+    @InjectMocks
+    private TrainerController trainerController ;
+    private ImageController imageController;
+
+    MockMvc mockMvc ;
 
     private HomeController homeController;
 
     @BeforeEach
     public void setup(){
 
-      // mockMvc= MockMvcBuilders.standaloneSetup(homeController).build();
+      mockMvc= MockMvcBuilders.
+              webAppContextSetup(webApplicationContext)
+              .apply(springSecurity())
+              .build();
 
 
     }
 
     @Test
-    public void testHomePage() throws Exception {
+    public void testHomePageMVCAuth_fail() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser("user")
+    public void testHomePageMVCAuth_success() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testHomePage() {
 
         homeController = new HomeController();
         String s = homeController.home();
@@ -33,14 +89,14 @@ public class HomeControllerTests {
         Assertions.assertEquals(s, "not-implemented");
     }
     @Test
-    public void testHomePageRegisterNotImplementedL() throws Exception {
+    public void testHomePageRegisterNotImplementedL(){
 
         homeController = new HomeController();
         String s = homeController.register();
         Assertions.assertEquals(s, "not-implemented");
     }
     @Test
-    public void testHomePageClassesNotImplemented() throws Exception {
+    public void testHomePageClassesNotImplemented() {
 
         homeController = new HomeController();
         String s = homeController.classes();
